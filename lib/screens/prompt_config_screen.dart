9e2e3157f1804befd86dafe8addff25e220eb0ef
Exception0x0194 <bricezhang0x0194@gmail.com>
2024-08-28 10:22:25 +0800

    删除了过去的 config widget

--- lib/screens/prompt_config_screen.dart
+++ lib/screens/prompt_config_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:nai_casrand/widgets/compact_prompt_config_widget.dart';
 import 'package:easy_localization/easy_localization.dart';
 
-import '../models/info_manager.dart';
 import '../widgets/prompt_config_widget.dart';
+import '../models/info_manager.dart';
 
 class PromptConfigScreen extends StatefulWidget {
   const PromptConfigScreen({super.key});
@@ -13,33 +12,21 @@ class PromptConfigScreen extends StatefulWidget {
 }
 
 class PromptConfigScreenState extends State<PromptConfigScreen> {
-  bool _showCompactView = true;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: _showCompactView
-            ? Column(children: [
-                ListTile(
-                    title: Text(context.tr('prompt_compact_view_hint')),
-                    dense: true),
-                CompactPromptConfigWidget(
-                    config: InfoManager().promptConfig, indentLevel: 0)
-              ])
-            : PromptConfigWidget(
-                config: InfoManager().promptConfig, indentLevel: 0),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: context.tr('toggle_compact_view'),
-        onPressed: () => {
-          setState(() {
-            _showCompactView = !_showCompactView;
-          })
-        },
-        child: _showCompactView
-            ? const Icon(Icons.visibility_off)
-            : const Icon(Icons.visibility),
+        child: Column(
+          children: [
+            ListTile(
+                title: Text(context.tr('prompt_compact_view_hint')),
+                dense: true),
+            CompactPromptConfigWidget(
+              config: InfoManager().promptConfig,
+              indentLevel: 0,
+            ),
+          ],
+        ),
       ),
     );
   }
